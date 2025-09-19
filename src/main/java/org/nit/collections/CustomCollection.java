14696bcd763c04db9084e5d37dd0359140894d92
vijay <dv9515615161@gmail.com>
2025-09-19 11:31:12 +0530

    intial commit

---
+++ src/main/java/org/nit/collections/CustomCollection.java
@@ -0,0 +1,34 @@
+package org.nit.collections;
+
+/**
+ * CustomCollection is a simple implementation of a collection that can hold both homogeneous and heterogeneous data types.
+ * It provides basic functionalities to add, remove, display, search, and update elements in the collection.
+ *
+ * @author Vijay Kumar
+ * @version 1.0
+ * @since 2025-09-19
+ */
+public class CustomCollection {
+
+    //creating array that can hold homogeneous and heterogeneous data
+    Object [] customArray;
+    //initializing size of array
+    int size = 10;
+    //creating array in constructor
+   public CustomCollection(){
+        customArray = new Object[size];
+    }
+    //initializing index
+    int index = 0;
+
+    //creating helper methods to add, remove, display, search and update elements in the array
+    /*
+      Adds an element to the collection.
+
+      @param obj The object to be added to the collection.
+     */
+    public void add(Object object){
+        customArray[index]=object;
+    }
+
+}
---
+++ src/main/java/org/nit/userclass/CustomCollectionImpl.java
@@ -0,0 +1,12 @@
+package org.nit.userclass;
+
+import org.nit.collections.CustomCollection;
+
+public class CustomCollectionImpl {
+    public static void main(String[] args) {
+        CustomCollection customCollection = new CustomCollection();
+        customCollection.add("Vijay");
+    }
+
+
+}
